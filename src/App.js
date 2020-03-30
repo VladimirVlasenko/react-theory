eb9c924aa2af0480a9d02107ef3318272ab6c717
Vladimir <79533330011@ya.ru>
2020-03-30 11:28:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = function() {
+  this.state = {
+    cars: [
+      {name: 'Ford', year: '2018'},
+      {name: 'Audi', year: '2016'},
+      {name: 'Mazda', year: '2010'}
+    ]
+  }
+    const divStyle = {
+      textAlign: 'center'
+    }
+  
+    const cars = this.state.cars
+    return (
+        <div style={divStyle}>
+          <h1>Hello world</h1>
+          <Car name={'cars[0].name'} year={cars[0].year}/>
+          <Car name={'cars[1].name'} year={cars[1].year}/>
+          <Car name={'cars[1].name'} year={cars[1].year}/>
+        </div>
+    )
+
+  
+
 }
 
 export default App;
